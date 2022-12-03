07d4bf3ada0f9d6b8817870a97c43dd57e2b38f8
wojtek-d <drabczykw1@gmail.com>
2022-12-03 20:45:23 +0100

    buggy commit

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,17 @@
 const { SlashCommandBuilder } = require("discord.js");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    await interaction.reply({ content: "Secret Pong!", ephemeral: true });
+    client.on(Events.InteractionCreate, async (interaction) => {
+      if (!interaction.isChatInputCommand()) return;
+
+      if (interaction.commandName === "ping") {
+        await interaction.reply({ content: "Secret Pong!", ephemeral: true });
+      }
+    });
   },
 };
